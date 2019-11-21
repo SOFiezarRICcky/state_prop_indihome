204ac038019801017ddda3b011c8042c82f41516
Sofiezar Ricky <sofiezar195@gmail.com>
2019-11-21 16:13:05 +0700

    State Prop Indihome FIX

--- src/props/Prop.jsx
+++ src/props/Prop.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import ReadMoreReact from 'read-more-react';
 
 const Isi = (props) => {
     return (
@@ -10,20 +11,23 @@ const Isi = (props) => {
                     <img src={props.img1} className="imageslider" />
                     <p className="tanggal">{props.tanggal1}</p>
                     <p className="judul">{props.judul1}</p>
-                    <p className="deskrip">{props.deskrip}</p>
+                    <p className="deskrip"><ReadMoreReact text={props.deskrip} min={10} ideal={20} max={25}
+                        readMoreText="Read More..." /></p>
                 </div>
                 <div className="bawahkiri">
                     <div className="bawah-kiri-kiri">
                         <img src={props.img2} className="imageslider" />
                         <p className="tanggal">{props.tanggal2}</p>
                         <p className="judul">{props.judul2}</p>
-                        <p className="deskrip">{props.deskrip}</p>
+                        <p className="deskrip"><ReadMoreReact text={props.deskrip} min={10} ideal={20} max={25}
+                            readMoreText="Read More..." /></p>
                     </div>
                     <div className="bawah-kiri-kanan">
                         <img src={props.img3} className="imageslider" />
                         <p className="tanggal">{props.tanggal3}</p>
                         <p className="judul">{props.judul3}</p>
-                        <p className="deskrip">{props.deskrip}</p>
+                        <p className="deskrip"><ReadMoreReact text={props.deskrip} min={10} ideal={20} max={25}
+                            readMoreText="Read More..." /></p>
                     </div>
                 </div>
             </div>
@@ -33,20 +37,23 @@ const Isi = (props) => {
                     <img src={props.img4} className="imageslider" />
                     <p className="tanggal">{props.tanggal4}</p>
                     <p className="judul">{props.judul4}</p>
-                    <p className="deskrip">{props.deskrip}</p>
+                    <p className="deskrip"><ReadMoreReact text={props.deskrip} min={10} ideal={20} max={25}
+                        readMoreText="Read More..." /></p>
                 </div>
                 <div className="bawahkanan">
                     <div className="bawah-kanan-kiri">
                         <img src={props.img5} className="imageslider" />
                         <p className="tanggal">{props.tanggal5}</p>
                         <p className="judul">{props.judul5}</p>
-                        <p className="deskrip">{props.deskrip}</p>
+                        <p className="deskrip"><ReadMoreReact text={props.deskrip} min={10} ideal={20} max={25}
+                            readMoreText="Read More..." /></p>
                     </div>
                     <div className="bawah-kanan-kanan">
                         <img src={props.img6} className="imageslider" />
                         <p className="tanggal">{props.tanggal6}</p>
                         <p className="judul">{props.judul6}</p>
-                        <p className="deskrip">{props.deskrip}</p>
+                        <p className="deskrip"><ReadMoreReact text={props.deskrip} min={10} ideal={20} max={25}
+                            readMoreText="Read More..." /></p>
                     </div>
                 </div>
             </div>
